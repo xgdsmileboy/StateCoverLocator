6a8d960c5e047a132c95542fbc95cf4c692d43f0
xgdsmileboy <xgd_smileboy@163.com>
2018-08-16 20:16:20 +0800

    add new predicate model

---
+++ src/locator/core/model/ClassifyPredicates.java
@@ -0,0 +1,20 @@
+package locator.core.model;
+
+import locator.common.java.Subject;
+
+public class ClassifyPredicates extends MLModel {
+
+	public final static String NAME = "classifier";
+	
+	protected ClassifyPredicates() {
+		super(NAME);
+		__name__ = "@ClassifyPredicates ";
+	}
+
+	@Override
+	public boolean modelExist(Subject subject) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
